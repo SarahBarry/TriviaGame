d017ce2043da8c05a5b0b166485985f96b98a06f
Sarah B <Realsarahbarry@gmail.com>
2018-09-04 23:26:08 -0400

    init

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# TriviaGame
+This is a trivia game using JavaScript for the logic and JQuery to manipulate the HTML.
+
+Instructions:
+
+Click Start to begin the game. You have 15 seconds to answer each question.
---
+++ assets/js/app.js
@@ -0,0 +1,226 @@
+// Define variables
+$(document).ready(function(){
+    var count = 0;
+    var time = 16;
+    var isSelected = false;
+    var ticker;
+    var correct = 0;
+    var incorrect = 0;
+    var unanswered = 0;
+
+// Questions and Answer Arrays
+    var question = ["How many ammendments are in The Bill of Rights?",
+    "Which of these Founding Fathers was not a president?", "How many Justices make up the Supreme Court?"];
+    var answer = ["10", "Benjamin Franklin", "9"];
+    var firstChoice = ["3", "John Adams", "3"];
+    var secondChoice = ["5", "Benjamin Franklin", "6"];
+    var thirdChoice = ["8", "George Washington", "9"];
+    var fourthChoice = ["10", "James Madison", "12"];
+
+// Show & Hide Functions
+    function showHolders() {
+        $("#question-holder").show();
+        $("#choice-holder-1").show();
+        $("#choice-holder-2").show();
+        $("#choice-holder-3").show();
+        $("#choice-holder-4").show();
+    }
+    function hideHolders() {
+        $("#question-holder").hide();
+        $("#choice-holder-1").hide();
+        $("#choice-holder-2").hide();
+        $("#choice-holder-3").hide();
+        $("#choice-holder-4").hide();
+    }
+    function hideResults() {
+        $("#correct-holder").hide();
+        $("#incorrect-holder").hide();
+        $("#unanswered-holder").hide();
+        $("#restart-holder").hide();
+    }
+    function displayQuestion () {
+        hideResults();
+        $("#answer-holder").hide();
+        $("#image-holder").hide();
+        $("#time-holder").show();
+        showHolders();
+        $("#question-holder").html(question[count]);
+        $("#choice-holder-1").html(firstChoice[count]);
+        $("#choice-holder-2").html(secondChoice[count]);
+        $("#choice-holder-3").html(thirdChoice[count]);
+        $("#choice-holder-4").html(fourthChoice[count]);
+    
+    // Hover CSS
+        $("#choice-holder-1").hover(function() {
+            $(this).css("color", "gray");
+        },
+        function(){
+            $(this).css("color", "black");
+        });
+        $("#choice-holder-2").hover(function() {
+            $(this).css("color", "gray");
+        },
+        function(){
+            $(this).css("color", "black");
+        });
+        $("#choice-holder-3").hover(function() {
+            $(this).css("color", "gray");
+        },
+        function(){
+            $(this).css("color", "black");
+        });
+        $("#choice-holder-4").hover(function() {
+            $(this).css("color", "gray");
+        },
+        function(){
+            $(this).css("color", "black");
+        });
+    }
+    $("#choice-holder-1").on("click", checkAnswer) 
+    $("#choice-holder-2").on("click", checkAnswer)
+    $("#choice-holder-3").on("click", checkAnswer)
+    $("#choice-holder-4").on("click", checkAnswer)
+
+// Check Answer Function
+    function checkAnswer() {
+
+        hideHolders();
+
+        if($(this).text() === answer[count]) {
+            stopTime();
+            isSelected = true;
+            $("#answer-holder").show();
+            $("#answer-holder").html("That's right! The answer is: " + answer[count]);
+            displayImage();
+            correct++;
+            count++;
+        }
+        else {
+            stopTime();
+            isSelected = true;
+            $("#answer-holder").show();
+            $("#answer-holder").html("Sorry! The answer is: " + answer[count]);
+            displayImage();
+            incorrect++;
+            count++;
+        } 
+
+        checkGameEnd();  
+    }
+
+// Chekc End Game Function
+    function checkGameEnd() {
+        if(count === question.length) {
+            $("#time-holder").hide();
+            showResults();
+            count = 0;
+            $(".start").show();
+            $(".start").on("click", function() {
+                resetResults();
+                startGame();
+            });
+        }
+    }
+
+    function resetTime() {
+        time = 16;
+    }
+
+    function displayTime() {
+        time--;
+        $("#time-holder").html("Time remaining: " + time);
+      
+            if(time <= 0) {
+                hideHolders();
+                stopTime();
+                $("#answer-holder").show();
+                $("#answer-holder").html("Time is up! The answer is: " + answer[count]);
+                displayImage();
+                unanswered++;
+                count++;
+                checkGameEnd();
+            }
+    }
+
+    function startTime() {
+        clearInterval(ticker);
+        ticker = setInterval(displayTime, 1000);
+    }
+    function stopTime() {
+        clearInterval(ticker);
+        resetTime();
+        if(count < question.length - 1) {
+            setTimeout(startTime, 2000);
+            setTimeout(displayQuestion, 3000);
+        }
+    }
+
+    resetTime();
+
+// Display Images With Answer
+    function displayImage() {
+        if(count === 0) {
+            $("#image-holder").show();
+            $("#image-holder").html('<img src="/Users/carlfowler/Desktop/TriviaGame/TriviaGame/assets/images/bill-of-rights.png">');
+        }
+        else if(count === 1) {
+            $("#image-holder").show();
+            $("#image-holder").html('<img src="/Users/carlfowler/Desktop/TriviaGame/TriviaGame/assets/images/benjamin-franklin.jpg">');
+        }
+        else if(count === 2) {
+            $("#image-holder").show();
+            $("#image-holder").html('<img src="/Users/carlfowler/Desktop/TriviaGame/TriviaGame/assets/images/supreme-court.jpg">');
+        }
+        else if(count === 3) {
+            $("#image-holder").show();
+            $("#image-holder").html('<img src="assets/images/merope_gaunt.png">');
+        }
+        else if(count === 4) {
+            $("#image-holder").show();
+            $("#image-holder").html('<img src="assets/images/snitch.jpg">');
+        }
+        else if(count === 5) {
+            $("#image-holder").show();
+            $("#image-holder").html('<img src="assets/images/phoenix.jpg">');
+        }
+        else if(count === 6) {
+            $("#image-holder").show();
+            $("#image-holder").html('<img src="assets/images/lion.jpg">');
+        }
+        else if(count === 7) {
+            $("#image-holder").show();
+            $("#image-holder").html('<img src="assets/images/neville_longbottom.jpg">');
+        }
+    }
+
+ // Show Results Function   
+    function showResults() {
+        $("#correct-holder").show();
+        $("#correct-holder").html("Correct: " + correct);
+        $("#incorrect-holder").show();
+        $("#incorrect-holder").html("Incorrect: " + incorrect);
+        $("#unanswered-holder").show();
+        $("#unanswered-holder").html("Unanswered: " + unanswered);
+        $("#restart-holder").show();
+        $("#restart-holder").html("Click Start above to play again!");
+    }
+
+// Reset Results Function 
+    function resetResults() {
+        correct = 0;
+        incorrect = 0;
+        unanswered = 0;
+    }
+
+// Start Game Function
+    function startGame() {
+        $(".start").hide();
+        startTime();
+        displayQuestion();
+    }
+
+// Start Game On Click
+  $(".start").on("click", function() {
+    startGame();
+  });
+});
